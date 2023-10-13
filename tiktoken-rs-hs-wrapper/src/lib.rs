64bd2b20fd2077f856ed9328b1b29a98aa1ac7b4
Alfredo Di Napoli <alfredo@well-typed.com>
2023-10-13 16:23:08 +0200

    Switch to cl100k_base

--- tiktoken-rs-hs-wrapper/src/lib.rs
+++ tiktoken-rs-hs-wrapper/src/lib.rs
@@ -1,7 +1,7 @@
 use haskell_ffi::from_haskell::marshall_from_haskell_var;
 use haskell_ffi::to_haskell::marshall_to_haskell_var;
 use std::marker::PhantomData;
-use tiktoken_rs::p50k_base;
+use tiktoken_rs::cl100k_base;
 
 pub enum TiktokenHs {}
 pub const TIKTOKEN_HS: PhantomData<TiktokenHs> = PhantomData;
@@ -10,7 +10,7 @@ pub const TIKTOKEN_HS: PhantomData<TiktokenHs> = PhantomData;
 pub extern "C" fn count_bpe_tokens(prompt: *const u8, prompt_len: usize) -> usize {
     let prompt: String = marshall_from_haskell_var(prompt, prompt_len, TIKTOKEN_HS);
 
-    let bpe = p50k_base().unwrap();
+    let bpe = cl100k_base().unwrap();
     let tokens = bpe.encode_with_special_tokens(&prompt);
     return tokens.len();
 }
@@ -23,7 +23,7 @@ pub extern "C" fn split_by_token(
     out_len: &mut usize,
 ) {
     let prompt: String = marshall_from_haskell_var(prompt, prompt_len, TIKTOKEN_HS);
-    let bpe = p50k_base().unwrap();
+    let bpe = cl100k_base().unwrap();
     let tokenized: Result<Vec<_>, _> = bpe.split_by_token_iter(&prompt, true).collect();
     let tokens = tokenized.unwrap();
     marshall_to_haskell_var(&tokens, out, out_len, TIKTOKEN_HS);
